f7a7b1494898a5c39a8a56fbb089cdbb6cdd9276
HCJ0HCJ <HCJHCJ0@163.com>
2018-09-29 10:22:47 +0800

    新建Server_2.1.x分支

--- old/eps_serversocket/src/main/java/com/jimi/smt/eps/serversocket/pack/BoardNumReplyPackage.java
+++
@@ -1,9 +0,0 @@
-package com.jimi.smt.eps.serversocket.pack;
-
-import cc.darhao.jiminal.annotation.Protocol;
-import cc.darhao.jiminal.core.BasePackage;
-
-@Protocol(0x42)
-public class BoardNumReplyPackage extends BasePackage {
-
-}
