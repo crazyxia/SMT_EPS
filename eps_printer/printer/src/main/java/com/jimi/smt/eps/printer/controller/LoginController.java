cf2c1410e0b9cfbe47aa1ba80ad5cfd5f2d650e2
HCJ0HCJ <HCJHCJ0@163.com>
2019-01-15 17:57:19 +0800

    SMT-Printer_1.11.1 update logs:
    完善代码，增加仓库领班、仓库主管两种用户类型

--- eps_printer/printer/src/main/java/com/jimi/smt/eps/printer/controller/LoginController.java
+++ eps_printer/printer/src/main/java/com/jimi/smt/eps/printer/controller/LoginController.java
@@ -66,6 +66,14 @@ public class LoginController implements Initializable {
 	 * SUPER_ADMINISTRATOR : 超级管理员
 	 */
 	private static final Integer SUPER_ADMINISTRATOR = 3;
+	/**
+	 * WAREHOUSE_SUPERVISOR : 仓库主管
+	 */
+	private static final Integer WAREHOUSE_SUPERVISOR = 7;
+	/**
+	 * WAREHOUSE_FOREMAN : 仓库领班
+	 */
+	private static final Integer WAREHOUSE_FOREMAN = 8;
 
 	
 	@Override
@@ -108,8 +116,7 @@ public class LoginController implements Initializable {
 						int resultCode = object.getInteger("result");
 						String userVOData = object.getString("data");
 						UserVO userVO = JSON.parseObject(userVOData, UserVO.class);
-						boolean isUserPermissionExist = userVO.getType().equals(WAREHOUSE_OPERATOR)
-								|| userVO.getType().equals(SUPER_ADMINISTRATOR);
+						boolean isUserPermissionExist = userVO.getType().equals(WAREHOUSE_OPERATOR) || userVO.getType().equals(SUPER_ADMINISTRATOR) || userVO.getType().equals(WAREHOUSE_FOREMAN) || userVO.getType().equals(WAREHOUSE_SUPERVISOR);
 						if (resultCode == 200 && isUserPermissionExist) {
 							// 成功登录
 							Platform.runLater(() -> {
